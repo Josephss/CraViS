91381f07acde57721e7ae9e9f2db4d8a80f5199c
Josephss <josephsolomon2010@gmail.com>
2017-09-23 00:12:36 +0000

    Now, the script graphs a data when it detects a file change

--- Visualize.py
+++ Visualize.py
@@ -10,9 +10,36 @@ import numpy as np
 import plotly.plotly as py
 import RPi.GPIO as GPIO
 
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 from plotly.graph_objs import Scatter, Layout, Figure
 from plotly import tools
 
+class Handler(FileSystemEventHandler):  
+    def process(self, event):
+        print event.src_path, event.event_type
+        grapher(event.src_path)
+    def on_modified(self, event):
+        self.process(event)
+    def on_created(self, event):
+        self.process(event)
+        
+def hasChanged():
+    print("Started monitoring and graphing data files. Press 'Ctrl + C' to terminate.")
+    event_handler = Handler()
+    observer = Observer()
+    observer.schedule(event_handler, path="data/", recursive=False)
+    observer.start()
+    
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Terminating  ...")
+        observer.stop()
+    observer.join()
+    
+    
 def isFloat(s):
     try: 
         float(s)
@@ -20,50 +47,26 @@ def isFloat(s):
     except ValueError:
         return False
     
-def csvReader(fileNameArr):    
-    #Store the time and temp values in a multi-dimentional array
-    n = len(fileNameArr)
-    time_temp = [[] for index in range(0,n*2)]
-    for i in range(n):
-        completePath = os.path.join("data/",fileNameArr[i]);
-        f = open(completePath)
-        csv_f = csv.reader(f)
-        for row in csv_f:
-            if(row != -1):
-                if(i>=1):
-                    time_temp[i+i].append(row[0])
-                    if(isFloat(row[1]) == True):
-                        time_temp[i+i+1].append(float(row[1]))
-                else:
-                    time_temp[i].append(row[0])
-                    if(isFloat(row[1]) == True):
-                        time_temp[i+1].append(float(row[1]))
-    return time_temp
+def csvReader(fileName):
+    sensorName = filter(str.isdigit,fileName)
+    time = []
+    temp = []
+    f = open(fileName)
+    csv_f = csv.reader(f)
+    for row in csv_f:
+        if(row != -1):
+            time.append(row[0])
+            if(isFloat(row[1]) == True):
+                temp.append(float(row[1]))
+    return sensorName, time, temp
 
 #Graph the temperature data from each sensor to their respective HTML file (sensor*.html) using the plotly library
-def grapher(csvArr, rate):
-    looper = True
-    while(looper):
-            try:
-                time_temp = csvReader(csvArr)[:]
-                n = len(time_temp)
-                traces_arr = [[] for index in range(0,n/2)]
-                for val in range(n/2):
-                	if(val>=1):
-                            tempName = Scatter(x=time_temp[val+val], y=time_temp[val+val+1], name = ("Detector" + str(val)))
-                            traces_arr[val].append(tempName)
-                	else:
-                            tempName = Scatter(x=time_temp[val], y=time_temp[val+1], name = ("Detector" + str(val)))
-                            traces_arr[val].append(tempName)
-                
-                for i in range(0, n/2):
-                    print("Writing out sensor " +str(i+1) + " data ...")
-                    plotly.offline.plot(traces_arr[i], filename = ("sensor" +str(i+1) + ".html"), auto_open=False, show_link=False)
-                    time.sleep(rate)
-                traces_arr[:]
-            except KeyboardInterrupt:
-                print("Terminating graphing ...")
-                looper = False
+def grapher(fileName):
+    trace = []   
+    sensor_time_temp = csvReader(fileName)[:]
+    trace.append(Scatter(x=sensor_time_temp[1], y=sensor_time_temp[2], name = ("Detector" + str(sensor_time_temp[0]))))
+    print("Writing out sensor " + str(sensor_time_temp[0]) + " data ...")
+    plotly.offline.plot(trace, filename = ("sensor" + str(sensor_time_temp[0]) + ".html"), auto_open=False, show_link=False)
     return
 
 #Config and start the python server to host the files from the directory
@@ -80,7 +83,7 @@ def server():
     return
 
 def main():
-    grapher(['sensor01.csv','sensor02.csv','sensor03.csv','sensor04.csv'], 5)
+    hasChanged()
 
 if __name__ == "__main__":
     main()
